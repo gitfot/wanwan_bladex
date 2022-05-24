505877c4e38f4ee9e6ccdc769369c2b689808cb0
zs-zhanglvxiu <p2wan@163.com>
2022-05-25 01:00:56 +0800

    排序算法

--- wanwan-demo/20220301-algorithms-collection/src/main/java/com/fun/collect/ga/srot/SortSolution.java
+++ wanwan-demo/20220301-algorithms-collection/src/main/java/com/fun/collect/ga/srot/SortSolution.java
@@ -4,4 +4,34 @@ package com.fun.collect.ga.srot;
  * @author wanwan 2022/5/19
  */
 public class SortSolution {
+
+	public int[] shellSort(int[] arr) {
+		int gap = arr.length / 2;
+		while (gap > 0) {
+			//插入排序
+			for (int i = gap; i < arr.length; i++) {
+				for (int j = i; j-gap > 0 && arr[j] < arr[j-gap]; j-=gap) {
+					int tmp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = tmp;
+				}
+			}
+			gap = gap / 2;
+		}
+		return arr;
+	}
+
+	/**
+	 * 找出数组中的重复数字
+	 */
+	public int findRepeatNumber(int[] nums) {
+
+	}
+
+	/**
+	 * 统计数组中的逆序对
+	 */
+	public int reversePairs(int[] nums) {
+
+	}
 }
