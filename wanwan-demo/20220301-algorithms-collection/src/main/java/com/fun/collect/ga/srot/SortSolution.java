da4ed06df809356e42456b2c2d88f5c5ea7f69f9
zs-zhanglvxiu <p2wan@163.com>
2022-05-26 15:33:14 +0800

    排序算法

--- wanwan-demo/20220301-algorithms-collection/src/main/java/com/fun/collect/ga/srot/SortSolution.java
+++ wanwan-demo/20220301-algorithms-collection/src/main/java/com/fun/collect/ga/srot/SortSolution.java
@@ -7,6 +7,12 @@ import java.util.Arrays;
  */
 public class SortSolution {
 
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
 	/**
 	 * 希尔排序 O(Nlog2N)
 	 */
@@ -68,10 +74,33 @@ public class SortSolution {
 	}
 
 	/**
-	 * 找出数组中的重复数字
+	 * 快速排序
 	 */
-	public int findRepeatNumber(int[] nums) {
-		return 0;
+	public static void quickSort(int[] arr, int l, int r) {
+		if (l >= r) {
+			return;
+		}
+		// i不能等于l+1，因为数组只有两个元素时 l+1不能正确交换
+		int i = l, j = r;
+		while (i < j) {
+			//这里得j--在i++前面，才能在第二个swap正确交换基点
+			while (i < j && arr[j] >= arr[l]) {
+				j--;
+			}
+			while (i < j && arr[i] <= arr[l]) {
+				i++;
+			}
+			swap(arr, i, j);
+		}
+		swap(arr, l, i);
+		quickSort(arr, l, i-1);
+		quickSort(arr, i+1, r);
+	}
+
+	public static void main(String[] args) {
+//		System.out.println(Arrays.toString(shellSort(new int[]{7,5,6,4})));
+//		int[] arr = new int[]{2,4,0,1,3,5}; quickSort(arr, 0, 5);
+//		System.out.println(Arrays.toString(arr));
 	}
 
 	/**
@@ -79,9 +108,5 @@ public class SortSolution {
 	 */
 	public int reversePairs(int[] nums) {
 		return 0;
-	}
-
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(shellSort(new int[]{7,5,6,4})));
 	}
 }
