754e0da74cbf680266b97023dfa9eaf5f2dba020
zs-zhanglvxiu <p2wan@163.com>
2022-06-08 17:55:29 +0800

    文件转化工具类完善

--- wanwan-demo/wanwan-web-common/src/main/java/com/fun/common/utils/utils/FileTransferUtils.java
+++ wanwan-demo/wanwan-web-common/src/main/java/com/fun/common/utils/utils/FileTransferUtils.java
@@ -1,35 +1,38 @@
 package com.fun.common.utils.utils;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 
 /**
- * @author zz
+ * @author zhanglvxiu
  * @date 2021/9/14
  */
+
+@Log4j2
 public class FileTransferUtils {
 
-	/*public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//获取类路径-spring自带
 		//输出：/E:/Desktop/SpringBlade/blade-demo/web-common-20210720/target/classes/
-		String cp1= ClassUtils.getDefaultClassLoader().getResource("").getPath();
+		//String cp1= ClassUtils.getDefaultClassLoader().getResource("").getPath();
 		//获取类路径-hutool
 		//输出：E:/Desktop/SpringBlade/blade-demo/web-common-20210720/target/classes/
-		String cp2 = ClassUtil.getClassPath();
-	}*/
-
-	private static final Logger logger = LoggerFactory.getLogger(FileTransferUtils.class);
+		//String cp2 = ClassUtil.getClassPath();
+//		MultipartFile multipartFile = urlToMultipartFile("http://192.168.30.226:9000/fhdi/upload/20220602/bc826f4eeed058219d4f2aed292e7c4a.png");
+//		System.out.println(multipartFile.getName());
+	}
 
 	/**
 	 * 下载文件
@@ -49,7 +52,7 @@ public class FileTransferUtils {
 		// 设置强制下载不打开
 		response.setContentType("application/force-download");
 		// 设置文件名
-//			response.addHeader("Content-Disposition", "attachment;fileName=" + filePath.substring(filePath.lastIndexOf(File.separator)));
+//		response.addHeader("Content-Disposition", "attachment;fileName=" + filePath.substring(filePath.lastIndexOf(File.separator)));
 		response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
 		//路径是否存在，不存在创建
 		File file = new File(filePath);
@@ -61,44 +64,83 @@ public class FileTransferUtils {
 		try (FileInputStream inputStream = new FileInputStream(filePath+fileName);
 			 ServletOutputStream outputStream = response.getOutputStream()){
 			IOUtils.copy(inputStream, outputStream);
-			logger.info("文件开始下载...");
+			log.info("文件开始下载...");
 			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
-			logger.info("文件下载失败...");
+			log.info("文件下载失败...");
 			return false;
 		}
 	}
 
-	/**
-	 * 将File对象转为MultipartFile
-	 * @param sourceDir 目标路径
-	 * @param sourceFile 目标文件名（带后缀）
-	 */
-	public static MultipartFile File2MultipartFile(String sourceDir, String sourceFile) {
+    /**
+     * 将url资源转换为MultipartFile
+     */
+	public static MultipartFile urlToMultipartFile(String url) {
+		File file;
+		MultipartFile multipartFile = null;
+		try {
+			HttpURLConnection httpUrl = (HttpURLConnection) new URL(url).openConnection();
+			httpUrl.connect();
+			file = inputStreamToFile(httpUrl.getInputStream(),"template.png");
+			multipartFile = file2MultipartFile(file);
+			httpUrl.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return multipartFile;
+	}
 
-		File targetFile = new File(sourceDir+sourceFile);
+	/**
+	 * 将本地文件转换为MultipartFile
+	 */
+	public static MultipartFile localToMultipartFile(String dir) {
+		File targetFile = new File(dir);
 		if (!targetFile.exists()) {
-			logger.error("未找到文件！");
+			log.error("未找到文件！");
 			return null;
 		}
+		return file2MultipartFile(targetFile);
+	}
+
+	/**
+	 * 将File对象转为MultipartFile
+	 */
+	public static MultipartFile file2MultipartFile(File targetFile) {
 		FileItem fileItem;
 		try {
 			fileItem = new DiskFileItem(targetFile.getName(), Files.probeContentType(targetFile.toPath()),
 				false, targetFile.getName(), (int) targetFile.length(), targetFile.getParentFile());
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.info("转MultipartFile失败,错误信息：", e);
 			return null;
 		}
 
 		try (InputStream input = new FileInputStream(targetFile);
 			 OutputStream os = fileItem.getOutputStream()) {
 			MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
-			org.apache.commons.io.IOUtils.copy(input, os);
+			IOUtils.copy(input, os);
 			return multipartFile;
 		} catch (Exception e) {
-			e.getStackTrace();
-			throw new RuntimeException("File转MultipartFile失败");
+			log.info("File转MultipartFile失败,错误信息：",e);
 		}
+		return null;
+	}
+
+	/**
+	 * inputStream 转 File
+	 */
+	public static File inputStreamToFile(InputStream ins, String name) throws Exception{
+		File file = new File(System.getProperty("java.io.tmpdir") + File.separator + name);
+		OutputStream os = new FileOutputStream(file);
+		int bytesRead;
+		int len = 8192;
+		byte[] buffer = new byte[len];
+		while ((bytesRead = ins.read(buffer, 0, len)) != -1) {
+			os.write(buffer, 0, bytesRead);
+		}
+		os.close();
+		ins.close();
+		return file;
 	}
 }
